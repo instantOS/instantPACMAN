8b43bc8a9b0dd3ce3633d69d5b0787d1b0dfa756
paperbenni <paperbenni@gmail.com>
2021-06-10 14:04:19 +0200

    add snap option

--- instantpacman.sh
+++ instantpacman.sh
@@ -52,12 +52,42 @@ by the chaotic and garuda team and is not connected to Arch Linux or instantOS.
 Enable now?' | imenu -C; then
             exit
         fi
-
-
+        # TODO actually install chaotic aur with imosid (once I get to writing it xD)
         ;;
-
+    *Snap)
+        SNAPOPTIONS=">>r Snap"
+        if systemctl is-enabled snapd; then
+            SNAPOPTIONS="$SNAPOPTIONS
+> Currently enabled
+:r 﨡Disable"
+        else
+            SNAPOPTIONS="$SNAPOPTIONS
+> Currently disabled
+:r 蘒Enable"
+        fi
+        CHOICE="$(echo "$SNAPOPTIONS" | instantmenu -bw 4 -w -1 -h -1 -i -q 'instantPACMAN' -c -l 30)"
+        [ -z "$CHOICE" ] && exit
+        case "$CHOICE" in
+        *Enable)
+            if ! {
+                echo 'warning: snaps come with significantly higher resource and storage usage'
+                echo 'than other solutions.  They also put no restrictions on low quality or broken'
+                echo 'applications and provide no way to distinguish them from working programs.  Use'
+                echo 'only if you have to.'
+            } | imenu -C; then
+                echo "good choice, but that's just my opinion"
+            fi
+            instantinstall snapd || exit
+            instantsudo systemctl enable --now snapd
+            imenu -m 'snap enabled. type snap in a terminal to get started'
+            ;;
+        *Disable)
+            instantsudo systemctl disable --now snapd
+            imenu -m 'snap disabled.'
+            ;;
+        esac
+        ;;
     esac
-
     ;;
 *close)
     exit
